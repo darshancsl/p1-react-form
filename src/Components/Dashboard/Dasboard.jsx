a2616ebc30895e918e23a9c203e55f554495ab1d
Coding Scale <codingscale157@gmail.com>
2023-08-05 18:13:18 +0530

    Adding AG grid for table

--- src/Components/Dashboard/Dasboard.jsx
+++ src/Components/Dashboard/Dasboard.jsx
@@ -1,50 +1,102 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Table } from "react-bootstrap";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const Dasboard = () => {
-
   const [userDetails, setUserDetails] = useState([]);
+  const gridRef = useRef();
+  const containerStyle = useMemo(() => ({ width: "100%", height: "70vh" }), []);
+  const gridStyle = useMemo(() => ({ height: "100%", width: "70vh" }), []);
 
-  const handleDelete = (e) => {
-    e.preventDefault();
-    const newUsers = userDetails;
-    const users = newUsers.filter(user => user.username !== e.target.id);
-    setUserDetails(users);
-    localStorage.setItem("userDetails", JSON.stringify(users));
-  }
+  const [columnDefs, setColumnDefs] = useState([
+    { field: "username", filter: true, minWidth: 80 },
+    { field: "email", filter: true, minWidth: 80 },
+    { field: "tags", minWidth: 80 },
+  ]);
+
+  const dataFromLS = useMemo(
+    () => JSON.parse(localStorage.getItem("userDetails")),
+    []
+  );
+
+  const defaultColDef = useMemo(
+    () => ({
+      sortable: true,
+      resizable: true,
+      floatingFilter: true,
+    }),
+    []
+  );
+
+  const onFirstDataRendered = useCallback(() => {
+    gridRef.current.api.sizeColumnsToFit();
+  }, []);
+
+  const onDeleteButtonClick = (rowId) => {
+    const updatedUserDetails = userDetails.filter(
+      (user) => user.username !== rowId
+    );
+    setUserDetails(updatedUserDetails);
+    localStorage.setItem("userDetails", JSON.stringify(updatedUserDetails));
+  };
+
+  const updatedColumnDefs = [
+    ...columnDefs,
+    {
+      headerName: "Action",
+      field: "action",
+      minWidth: 100,
+      cellRenderer: (params) => {
+        const { username } = params.data;
+        return (
+          <button
+            style={{
+              backgroundColor: "red",
+              padding: "0px 10px",
+              fontWeight: "bold",
+              color: "white",
+              border: "none",
+              cursor: "pointer",
+            }}
+            onClick={() => onDeleteButtonClick(username)}
+          >
+            Delete
+          </button>
+        );
+      },
+    },
+  ];
 
   useEffect(() => {
-    setUserDetails(JSON.parse(localStorage.getItem('userDetails')))
-  }, [setUserDetails])
+    setUserDetails(dataFromLS);
+  }, [dataFromLS]);
 
   return (
     <>
-      <h1 className="fw-bold text-center py-5">User Details</h1>
-      <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>UserName</th>
-          <th>Email</th>
-          <th>Tags</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          userDetails?.map(({ username, email, tags}) => {
-            return (
-              <tr key={username}>
-                <td>{username}</td>
-                <td>{email}</td>
-                <td>{tags.map(el => '#'+el + ' ')}</td>
-                <td><Button variant="danger" onClick={handleDelete} id={username}>Delete</Button></td>
-              </tr>
-            )
-          })
-        }
-      </tbody>
-    </Table>
+      <h1 className='fw-bold text-center py-5'>User Details</h1>
+      {userDetails && (
+        <div className='ag-theme-alpine' style={containerStyle}>
+          <AgGridReact
+            style={gridStyle}
+            ref={gridRef}
+            columnDefs={updatedColumnDefs}
+            defaultColDef={defaultColDef}
+            rowData={userDetails}
+            onFirstDataRendered={onFirstDataRendered}
+            animateRows={true}
+            rowSelection='multiple'
+          />
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Dasboard
+export default Dasboard;
